ab6fd410d5b7f68f0daa13377d2130e4bd6d00df
roddamatthew <rodda.matthew1@gmail.com>
2025-12-16 20:51:44 +1030

    clean up chunk struct

--- src/hpoc.c
+++ src/hpoc.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <sys/mman.h>
 #include "hpoc.h"
 
@@ -8,26 +9,20 @@
 typedef struct {
     size_t prev_size;
     size_t size : 61;
-    size_t NON_MAIN_ARENA : 1;
-    size_t IS_MMAPED : 1;
-    size_t PREV_INUSE : 1;
-    char data[];
-}a_chunk;
+    bool NON_MAIN_ARENA : 1;
+    bool IS_MMAPED : 1;
+    bool PREV_INUSE : 1;
+    chunk *fd;              // Only used if chunk is free
+    chunk *bk;              // Only used if chunk is free
+    chunk *fd_nextsize;     // Only used if chunk is free
+    chunk *bk_nextsize;     // Only used if chunk is free
+}chunk;
+
+#define chunk2mem(p)    (void *)((char *)(p) + 2 * sizeof(size_t))
+#define mem2chunk(p)    (chunk *)((char *)p - 2 * sizeof(size_t))
 
 typedef struct {
-    size_t prev_size;
-    size_t size : 61;
-    size_t NON_MAIN_ARENA : 1;
-    size_t IS_MMAPED : 1;
-    size_t PREV_INUSE : 1;
-    f_chunk *fd;
-    f_chunk *bk;
-    size_t fd_nextsize;
-    size_t bk_nextsize;
-}f_chunk;
-
-typedef struct {
-    f_chunk *bins[8];
+    chunk *bins[8];
 }TCache;
 TCache tcache; // fd only
 
@@ -36,23 +31,24 @@ typedef struct {
     // FastBins fast_bins;
     // SmallBins small_bins;
     // LargeBins large_bins;
-    f_chunk *unsorted_bin;
-    a_chunk top_chunk;
+    chunk *unsorted_bin;
+    chunk *top_chunk;
 }arena;
 
 static arena main_arena = {0};
 
-
 void hpoc_init() {
     char *ptr = (char *)mmap(0, INITIAL_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    main_arena.top_chunk = (a_chunk) {0, INITIAL_SIZE, 0, 0, 1};
-
     
+    main_arena.top_chunk = (chunk *)ptr;
+    main_arena.top_chunk->size = INITIAL_SIZE;
+    main_arena.top_chunk->IS_MMAPED = true;
+    main_arena.top_chunk->PREV_INUSE = true;
 }
 
 void *malloc(size_t size) {
     // If the top chunk is out of memory
-    if (main_arena.top_chunk.size == 0)
+    if (main_arena.top_chunk->size == 0)
         hpoc_init();
 
     // If the tcache for the requested size isn't empty, return the HEAD
@@ -71,18 +67,18 @@ void *malloc(size_t size) {
     
 }
 
-void free(void *ptr) {
-    f_chunk* chunk = (f_chunk *)ptr - 16;
+// void free(void *ptr) {
+//     chunk* chunk = (f_chunk *)ptr - 16;
     
-    // Place in tcache if size fits and there's room
+//     // Place in tcache if size fits and there's room
 
-    // Place in fastbin if size fits
+//     // Place in fastbin if size fits
 
-    // Attempt to consolidate forwards and backwards
+//     // Attempt to consolidate forwards and backwards
 
-    // Place at the HEAD of the unsorted list to sorted later
-    chunk->fd = main_arena.unsorted_bin;
-    chunk->bk = NULL;
-    main_arena.unsorted_bin->bk = chunk;
-    main_arena.unsorted_bin = chunk;
-}
+//     // Place at the HEAD of the unsorted list to sorted later
+//     chunk->fd = main_arena.unsorted_bin;
+//     chunk->bk = NULL;
+//     main_arena.unsorted_bin->bk = chunk;
+//     main_arena.unsorted_bin = chunk;
+// }
